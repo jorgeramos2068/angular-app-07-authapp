35ea49aa72e92d4854caf5e8fc27d86ace2135ea
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:02:52 -0600

    Implement reactive forms configuration to login and register components

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public form: FormGroup = this.formBuilder.group({
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  login(): void {
+    console.log(this.form.value);
+    console.log(this.form.valid);
   }
-
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  public form: FormGroup = this.formBuilder.group({
+    name: ['Test 1', [Validators.required]],
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  register(): void {
+    console.log(this.form.value);
   }
-
 }
